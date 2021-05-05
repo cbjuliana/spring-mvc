92e8f471209443efdbb319b5ada7fe8786fc69ec
Juliana <cbatista.juliana@gmail.com>
2021-05-05 19:56:40 -0300

    Adicionando projeto Spring MVC

---
+++ mudi/src/main/java/org/example/mudi/controller/HelloController.java
@@ -0,0 +1,19 @@
+package org.example.mudi.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+	@GetMapping("/hello")
+	public String hello(HttpServletRequest request) {
+		request.setAttribute("nome", "Mundo");
+		return "hello";
+	}
+	
+	
+	
+}
